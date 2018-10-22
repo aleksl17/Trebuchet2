b35eb549865ff3b231e0a16d4a187a1f2cf2b9c5
Aleksander M Lingstad <aleksl17@uia.no>
2018-10-22 18:19:32 +0200

    Added ReadMe and credits

--- Trebuchet2/main.cpp
+++ Trebuchet2/main.cpp
@@ -3,14 +3,26 @@
 
 #include "game.h"
 
-/* ========================= MUST be done before running! =========================
- * Set working directory: Run -> Edit Configurations... -> set "working directory:" to projects current directory, e.g. "C:\Users\*USERNAME*\trebuchet2\Trebuchet2"
+/* ============================== MUST be Done before Running! ==============================
  * Configure SFML (https://goo.gl/1Zmgb2)
  * Configure JSONCPP (https://goo.gl/G9LikB)
- * ========================= Should be done =========================
+ * * Set working directory: Run -> Edit Configurations... -> set "working directory:" to projects current directory, e.g. "C:\Users\*USERNAME*\trebuchet2\Trebuchet2"
+ * ============================== Should be Done Before Running ==============================
  * Skim through "sfml_tiled" found here: https://goo.gl/dN8u6b (required login)
  * Skim through "Basic Game Engine" from the SFML devs, found here: https://goo.gl/vbxt5Z
-*/
+ * ============================== Documentation and Help ==============================
+ * https://github.com/SFML/SFML (Wiki)
+ * https://www.sfml-dev.org/
+ * https://github.com/open-source-parsers/jsoncpp
+
+
+ * PROJECT TREBUCHET 2: TITLE IN PROGRESS
+ * CREATED BY:
+ * Hassan Mohammed Ali
+ * Håvard Friborg
+ * Aleksander Lingstad
+ * Abdelraouf Tria
+ */
 
 int main() {
     Game game;
